2222b76e3aed1fca76345f985040ca99fb9bdc2a
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 14:57:53 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -30,7 +30,7 @@ export default function Intro() {
     <a href="#"
                    class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Website Disclaimer</a>
             </div>
-        </div>
+        
     
     </nav>
 
