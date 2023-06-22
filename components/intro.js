eaf35e26bb973924c95f53100f12a290f3eab212
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 16:00:43 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
 <nav
-        class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
+        class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-violet-700">
         <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
                 <img class="w-44" src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' />
@@ -17,11 +17,11 @@ export default function Intro() {
             
             <div class="flex ">
                 <a href="#"
-                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Sign
+                   class="block text-md px-4 py-2 rounded text-violet-700 ml-2 font-bold hover:text-white mt-4 hover:bg-violet-700 lg:mt-0">Sign
                     in</a>
     
                 <a href="#"
-                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">login</a>
+                   class=" block text-md px-4  ml-2 py-2 rounded text-violet-700 font-bold hover:text-white mt-4 hover:bg-violet-700 lg:mt-0">login</a>
             </div>
         </div>
     
