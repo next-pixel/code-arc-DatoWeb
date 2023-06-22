82374b46993715140e61524c98dca118ef703712
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 15:44:12 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,8 +1,3 @@
 export default function Intro() {
-  return (
-
-                <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' style="width: 20%;" />
-           
-
-  );
+  return ();
 }
