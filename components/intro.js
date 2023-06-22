bd00da27c51e384ff9f78490173bb434e7b27c1f
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 15:40:16 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,26 +1,8 @@
 export default function Intro() {
   return (
-<nav class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
-        <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
-            <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
+
                 <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' style="width: 20%;" />
-            </div>
            
-        </div>
-    
-        
-            
-            
-            <div class="flex ">
-                <a href="#"
-                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">About </a>
-    
-                <a href="#" class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Privacy Policy</a>
-    <a href="#" class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Website Disclaimer</a>
-            </div>
-        
-    
-    </nav>
 
   );
 }
