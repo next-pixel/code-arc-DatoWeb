0dfeb78f411cec7d2cd6ff1becb255305847d340
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 15:54:17 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -4,13 +4,15 @@ export default function Intro() {
         class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
         <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' style='width:20%'/>
+                <img class="w-44" src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' />
             </div>
             
         </div>
     
         <div class="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-           
+            <div class="text-md font-bold text-blue-700 lg:flex-grow">
+               
+            </div>
             
             
             <div class="flex ">
