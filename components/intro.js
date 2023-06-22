fab4b5561af58cba905a06effa17c8a780547a14
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 14:55:41 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -2,9 +2,9 @@ export default function Intro() {
   return (
 <nav
         class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
-        <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+        <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png'/>
+                <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' style="width: 20%;" />
             </div>
             <div class="block lg:hidden ">
                 <button
@@ -17,30 +17,18 @@ export default function Intro() {
             </div>
         </div>
     
-        <div class="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-            <div class="text-md font-bold text-blue-700 lg:flex-grow">
-                <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                    Menu 1
-                </a>
-                <a href="#responsive-header"
-                   class=" block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                    Menu 2
-                </a>
-                <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                    Menu 3
-                </a>
-            </div>
+        
             
             
             <div class="flex ">
                 <a href="#"
-                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Sign
+                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">About
                     in</a>
     
                 <a href="#"
-                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">login</a>
+                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Privacy Policy</a>
+    <a href="#"
+                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Website Disclaimer</a>
             </div>
         </div>
     
