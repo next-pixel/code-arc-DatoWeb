59f912ba11bedc8762cfd741d82cda460f451337
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 14:26:31 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     
 <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">Code Arc™</a>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
@@ -15,7 +15,7 @@ export default function Footer() {
             <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
         </li>
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Website Disclaimer</a>
         </li>
         <li>
             <a href="#" class="hover:underline">Contact</a>
