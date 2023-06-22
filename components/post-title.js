5eb32202c7afafcc59fb13c1346f346c0c362329
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 17:18:39 +0530

    Update post-title.js

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-6xl md:text-6xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-5xl md:text-5xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
     </h1>
   );
