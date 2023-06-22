295d5e0f2dcaae52588b6db2f8da906e741499b3
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 15:38:37 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,20 +1,11 @@
 export default function Intro() {
   return (
-<nav
-        class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
+<nav class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
         <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
                 <img src='https://www.datocms-assets.com/103068/1687417296-code-arc3-1.png' style="width: 20%;" />
             </div>
-            <div class="block lg:hidden ">
-                <button
-                    id="nav"
-                    class="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700">
-                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                    </svg>
-                </button>
-            </div>
+           
         </div>
     
         
@@ -22,13 +13,10 @@ export default function Intro() {
             
             <div class="flex ">
                 <a href="#"
-                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">About
-                    in</a>
+                   class="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">About </a>
     
-                <a href="#"
-                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Privacy Policy</a>
-    <a href="#"
-                   class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Website Disclaimer</a>
+                <a href="#" class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Privacy Policy</a>
+    <a href="#" class=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Website Disclaimer</a>
             </div>
         
     
