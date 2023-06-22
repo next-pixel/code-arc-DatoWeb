47f2e30c7bd73c7bad16d11b033f21bf95520ebe
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 13:44:24 +0530

    Update post-header.js

--- components/post-header.js
+++ components/post-header.js
@@ -6,7 +6,7 @@ import PostTitle from "./post-title";
 export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
-     <div className="max-w-2xl mx-auto">
+     <div className="max-w-5xl mx-auto">
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
