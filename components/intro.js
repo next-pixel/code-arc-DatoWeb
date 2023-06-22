c44ae92a723f282147322acbf6e82f4928e5fc85
Krunal Shah <68117659+next-pixel@users.noreply.github.com>
2023-06-22 14:37:42 +0530

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,7 +1,5 @@
 export default function Intro() {
   return (
-   
-<!-- component -->
 <nav
         class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
         <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
