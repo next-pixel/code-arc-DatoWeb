6c987a423dd1920850f05f41ad4eaec7134e844d
Krunal Shah <krunal.shah@clariontech.com>
2023-06-29 21:13:02 +0530

    change in header

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ export default function Intro({ mqposts }) {
   return (
 <nav
         className="flex items-center justify-between flex-wrap bg-white  lg:px-12 shadow border-solid border-t-2 border-indigo-400">
-        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-0 lg:pb-0">
             <div className="flex items-center flex-shrink-0 text-gray-800 mr-16"> <a href="/" title="Code Arc">
             <Image
       data={{
@@ -13,7 +13,7 @@ export default function Intro({ mqposts }) {
         alt: `Code Arc Image Logo`,
         title:`Code Arc Image Logo`,
         width:200,
-        height:60
+        height:50
       }}
     />
                 </a>
